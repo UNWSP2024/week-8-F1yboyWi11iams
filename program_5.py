de9cd428d118fd9314e91524ac0971764b240321
Seth J. Williams <sjwilliams3@students.unwsp.edu>
2024-10-26 00:50:50 -0500

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -3,3 +3,26 @@
 # For example a course ID could be "COS 2005" and the course name could be "Python Programming."   
 # Then ask the user for a subject (like "COS"). 
 # Finally, the program will display the ID and name of all the courses having that subject.
+def main():
+	def course_list():
+		list = []
+		input("Please input a course name and ID: ")
+  		again = 'y'
+  		again = input('y = yes, anything else = no: ')
+	course_list():
+
+
+
+	list = []
+	int(input("Enter the subject name and the corresponding ID number: ))
+
+
+def all_values(list):
+	general_course_list = for list in range(input("Please enter the subject name: "))
+
+	print(general_course_list)
+
+
+	all_values(list):
+if __name__ == "__main__"
+	main():
